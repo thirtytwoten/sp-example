e44b138a2f4cf58b472f8e6b6d4f9efddca8f325
Polly Pocket <pp@lawn-128-61-41-114.lawn.gatech.edu>
2018-03-12 14:33:31 -0400

    add simple peer example code

---
+++ script.js
@@ -0,0 +1,22 @@
+//var Peer = require('simple-peer') // if you're using browserfy
+var p = new SimplePeer({ initiator: location.hash === '#1', trickle: false })
+
+p.on('error', function (err) { console.log('error', err) })
+
+p.on('signal', function (data) {
+  console.log('SIGNAL', JSON.stringify(data))
+  document.querySelector('#outgoing').textContent = JSON.stringify(data)
+})
+
+document.querySelector('form').addEventListener('submit', function (ev) {
+  ev.preventDefault()
+  p.signal(JSON.parse(document.querySelector('#incoming').value))
+})
+
+p.on('connect', function () {
+  console.log('CONNECTED')
+})
+
+p.on('data', function (data) {
+  console.log('data: ' + data)
+})
